9078139d6513a29f6080ffc5aad5680b402165ff
Ex-libris <etellion@gmail.com>
2025-08-29 09:46:28 +0100

    change order of the dictionary

--- gui/common.py
+++ gui/common.py
@@ -5,21 +5,21 @@ from PyQt5 import QtWidgets, QtGui
 
 # ---------------- Parameter registry ----------------
 PARAMS_BASE = [
+    ("amp_ref", "EDIT", "Edit23", "Amplitude Ref", True), #guarded
     ("amp_ki", "EDIT", "Edit24", "Amplitude Ki", False),
     ("amp_kp", "EDIT", "Edit32", "Amplitude Kp", False),
     ("pll_kp", "EDIT", "Edit27", "PLL Kp", False),
     ("pll_ki", "EDIT", "Edit22", "PLL Ki", False),
-    ("amp_ref", "EDIT", "Edit23", "Amplitude Ref", True),   # guarded
     ("used_freq", "DNC", 3, "Used Frequency (f₀)", False),
     ("drive", "DNC", 4, "Drive", True),                    # guarded
 ]
 
 PARAM_TOOLTIPS = {
+    "amp_ref": "Target oscillation amplitude (units follow SXM).",
     "amp_ki": "Amplitude loop integral gain...",
     "amp_kp": "Amplitude loop proportional gain...",
     "pll_kp": "PLL proportional gain...",
     "pll_ki": "PLL integral gain...",
-    "amp_ref": "Target oscillation amplitude (units follow SXM).",
     "used_freq": "PLL used frequency (actual f₀, SXM units).",
     "drive": "Drive amplitude to sustain oscillation (SXM units).",
 }
